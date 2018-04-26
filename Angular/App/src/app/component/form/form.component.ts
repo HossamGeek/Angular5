18a86835a535db0d24f403d17b8b7a900c2b03a9
Hossam Mahmoud <hossam_geekit@yahoo.com>
2018-04-26 12:00:21 +0200

    update firbase to get data with key id

--- Angular/App/src/app/component/form/form.component.ts
+++ Angular/App/src/app/component/form/form.component.ts
@@ -1,9 +1,11 @@
+///<reference path="../../../../node_modules/angularfire2/database-deprecated/firebase_list_observable.d.ts"/>
 import { Component, OnInit } from '@angular/core';
 import {DataServiceService } from '../../services/data-service.service';
 
 import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 import {Observable} from 'rxjs/Observable';
 import {Employee} from '../interfaces/Employee';
+import 'rxjs/add/operator/map';
 import {FirebaseObjectObservable, FirebaseListObservable} from 'angularfire2/database-deprecated';
 import * as firebase from 'firebase';
 import {hasKey} from 'angularfire2/database-deprecated/utils';
@@ -20,9 +22,10 @@ import {getOverloadKey} from 'tslint/lib/rules/adjacentOverloadSignaturesRule';
 export class FormComponent implements OnInit {
 
   employess: AngularFireList<any>;
+  employess1: AngularFireList<any>;
   allemp: Observable<any[]>;
   allempdata: Observable<any[]>;
-
+  item: Observable<Employee[]>;
   pushUser = {};
 
   users: Employee = {
@@ -41,27 +44,40 @@ export class FormComponent implements OnInit {
 
   alluser: Employee[];
   alluser2: Employee[];
-  constructor(public  dataService: DataServiceService, public db: AngularFireDatabase) {
+  constructor(public  dataService: DataServiceService, public db: AngularFireDatabase ) {
     /*this.dataService.getUser().subscribe(alluser => {
       this.alluser = alluser;
     });*/
 
         this.employess =  db.list('/employees') ;
 
-        this.allemp = this.employess.snapshotChanges();
-        this.allempdata = this.employess.valueChanges();
-          this.allemp.forEach(element => {
-           this.alluser = element;
-          console.log(element);
+        this.allemp = this.employess.snapshotChanges().map(actions => {
+
+          return actions.map(a => {
+            const k = a.payload.key.toString();
+            const p = a.payload.toJSON();
+            return {k, p};
           });
-    this.allempdata.forEach(element => {
-      this.alluser2 = element;
-      console.log(element);
-    });
+
+        });
+          this.allemp.forEach(element => {
+            this.alluser = element;
+            console.log(this.alluser);
+          });
+
+        /*this.allempdata = this.employess.valueChanges();
+*/
+
+/*          this.allempdata.forEach(element => {
+            this.alluser2 = element;
+           // console.log(element);
+          });*/
   }
 
   ngOnInit() {
   }
+
+
 
   submit({value, valid}) {
 
@@ -106,6 +122,6 @@ export class FormComponent implements OnInit {
 
   delete(value) {
     this.employess.remove(value);
-}
+  }
 
 }
